d85a219f0d6dc95a47c69a5e34ad4047a76b88e1
Camila <camila.rodriguez.aguila@gmail.com>
2023-10-29 08:40:51 -0700

    Comentar código y Reformatear

--- Busqueda.java
+++ Busqueda.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+//Métoodo para añadir aerolíneas
 public class Busqueda {
     public static void Interaccion(){
         System.out.println("=== Bienvenid@ al sistema de búsqueda de vuelos baratos ===");
@@ -11,13 +12,13 @@ public class Busqueda {
         aceptarArchivos();
 
         int repeticion = insertarMasAerolineas();
-        while (repeticion!=0){
+        while (repeticion != 0){
             if (repeticion == -1){
-                repeticion = insertarMasAerolineas();
+                repeticion = insertarMasAerolineas(); // Volver a preguntar si la entrada no es válida
             }
 
             else if (repeticion == 1){
-                aceptarArchivos();
+                aceptarArchivos(); // Añadir más aerolíneas
                 repeticion = insertarMasAerolineas();
             }
         }
@@ -28,17 +29,18 @@ public class Busqueda {
         System.out.println("¿Desea ingresar otra aerolínea? (S/N): ");
         String respuesta = in.nextLine();
         if (respuesta.equalsIgnoreCase("S")){
-            return 1;
+            return 1; // Agregar más aerolíneas
         }
         else if (respuesta.equalsIgnoreCase("N")){
-            return 0;
+            return 0; // No agregar más aerolíneas, finalizar
         }
         else {
             System.out.println("Por favor, ingrese S para ingresar más y N para no: ");
-            return -1;
+            return -1; // Entrada no válida, volver a preguntar
         }
     }
 
+    //Metodo de Validaciones
     public static void aceptarArchivos(){
         int validacion = 0;
         String aerolinea;
@@ -46,7 +48,8 @@ public class Busqueda {
         do{
             System.out.println("Ingrese el nombre de una Aerolínea: ");
             aerolinea = in.nextLine();
-        } while (aerolinea.length()==0);
+        } while (aerolinea.length() == 0); // Asegurarse de que se ingrese un nombre de aerolínea válido
+
         do{
             validacion = 0;
             System.out.println("Ingrese el nombre de archivo que contiene la información de los vuelos de " + aerolinea + ": ");
@@ -57,9 +60,9 @@ public class Busqueda {
                 BufferedReader br = new BufferedReader (fr);
                 System.out.println("Se ha cargado la información. \n");
             } catch (Exception e){
-                System.out.println(e.getMessage());
-                validacion = 1;
+                System.out.println(e.getMessage()); // Manejar errores al abrir archivos
+                validacion = 1; // Marcador de error, volver a preguntar
             }
-        } while (validacion>=1);
+        } while (validacion >= 1); // Repetir hasta que se ingrese un archivo válido
     }
 }
