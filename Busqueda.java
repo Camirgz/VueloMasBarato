57aeadb597c6eb1e2dca04767764b5c88bc059f1
Camila <camila.rodriguez.aguila@gmail.com>
2023-10-30 22:00:25 -0700

    eliminar linea

--- Busqueda.java
+++ Busqueda.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileReader;
 import java.io.IOException;
 import javax.swing.JFileChooser;
 import java.util.Scanner;
