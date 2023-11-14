6a2fd7698a9a10a6f3cef312730b87a30921c090
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2023-11-13 18:17:07 -0600

    Update Aerolinea.java

--- Aerolinea.java
+++ Aerolinea.java
@@ -1,68 +1,30 @@
 public class Aerolinea{
     private String nombre;
-    private String [] ciudadesOperando = new String [1000];
-    private int cantidadCiudadesOperando;
-    private Vuelo [] vuelos;
-    private Vuelo [][][] vuelos_relacionados;
+    private String [] ciudadesOperando;
+    private String [] vuelos;
+    private Vuelo [][][] matriz;
     
-    public Aerolinea (String nombre, String [] informacion){
-        String ciudadesOperando = "";
-        vuelos = new Vuelo [150];
-        
-        for (int i = 0; i<informacion.length; i++){
-            String [] datos = informacion[i].split(",");
-            
-            String precio_str = datos[0];
-            double precio = Double.parseDouble(precio_str);
-            String ciudadSalida = datos[1];
-            ciudadesOperando = ciudadesOperando + "," + ciudadSalida;
-            String ciudadLlegada = datos[2];
-            String horaSalida = datos[3];
-            String horaLlegada = datos[4];
-            String diaSalida = datos[5];
-            String diaLlegada = datos[6];
-            String codigoVuelo = datos[7];
-            String asientosDisponibles_str = datos[8];
-            int asientosDisponibles = Integer.parseInt(asientosDisponibles_str);
-            
-            this.nombre = nombre;
-            vuelos[i] = new Vuelo (precio, ciudadSalida, ciudadLlegada, horaSalida, horaLlegada, diaSalida, diaLlegada, codigoVuelo, asientosDisponibles);
+    public Aerolinea(String nombre, String [] datos){
+        this.nombre = nombre;
+        this.vuelos = new String [datos.length];
+        for (int i = 0; i<datos.length; i++){
+            vuelos[i] = datos[i];
         }
         
-        String [] ciudades_temporales = ciudadesOperando.split(",");
-        for (int i = 0; i<ciudades_temporales.length; i++){
-            if (buscadorCiudades(ciudades_temporales[i]) == false){
-                this.cantidadCiudadesOperando++;
-            }
-        }
+        //sacar ciudades operando
+    }
+    
+    public void CargarMatriz(){
+        matriz = new Vuelo [ciudadesOperando.length][ciudadesOperando.length][150];
         
-        this.ciudadesOperando = new String [this.cantidadCiudadesOperando];
-        for (int i = 0; i<ciudades_temporales.length; i++){
-            if (ciudadesOperando != null && buscadorCiudades(ciudades_temporales[i]) == false){
-                this.ciudadesOperando[i] = ciudades_temporales[i];
-            }
-        }
+        
     }
     
-    public boolean buscadorCiudades(String ciudad){
-        boolean existe = true;
-        for (int i = 0; i<ciudadesOperando.length; i++){
-            if (ciudad.equals(ciudadesOperando[i])){
-                existe = true;
-                break;
-            }
-            else {
-                existe = false;
-            }
-        }
-        return existe;
+    public void VueloDirecto(){
+        
     }
     
-    public Vuelo[] getVuelos (){
-        return this.vuelos;
-    }
-    
-    public String getNombre(){
-        return this.nombre;
+    public void VueloEscalas(){
+        
     }
 }
