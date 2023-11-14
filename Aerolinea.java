f4ff30d09137b6801ffc83d884a6e596dd1e2c46
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2023-11-13 21:54:25 -0600

    Update Aerolinea.java

--- Aerolinea.java
+++ Aerolinea.java
@@ -4,24 +4,103 @@ public class Aerolinea{
     private String [] vuelos;
     private Vuelo [][][] matriz;
     
-    public Aerolinea(String nombre, String [] datos){
+        public Aerolinea(String nombre, String[] datos) {
         this.nombre = nombre;
-        this.vuelos = new String [datos.length];
-        for (int i = 0; i<datos.length; i++){
-            vuelos[i] = datos[i];
+        this.vuelos = new String[datos.length];
+        String ciudadesOperandoStr = "";
+
+        for (int i = 0; i < datos.length; i++) {
+            this.vuelos[i] = datos[i];
+            String[] partes = datos[i].split(",");
+            ciudadesOperandoStr = agregarCiudad(ciudadesOperandoStr, partes[1]);
+            ciudadesOperandoStr = agregarCiudad(ciudadesOperandoStr, partes[2]);
         }
+
+        // Convertir ciudadesOperandoStr a un array
+        ciudadesOperando = ciudadesOperandoStr.split(",");
         
-        //sacar ciudades operando
+        CargarMatriz();
+    }
+
+    private String agregarCiudad(String ciudadesStr, String ciudad) {
+        // Verificar si la ciudad ya está en ciudadesOperando
+        if (contieneCiudad(ciudadesStr, ciudad)==false) {
+            ciudadesStr += ciudad + ",";
+        }
+        return ciudadesStr;
+    }
+
+    private boolean contieneCiudad(String ciudadesStr, String ciudad) {
+        // Verificar si ciudadesStr contiene la ciudad
+        String[] ciudades = ciudadesStr.split(",");
+        for (String c : ciudades) {
+            if (c!=null && c.equals(ciudad)) {
+                return true;
+            }
+        }
+        return false;
     }
     
     public void CargarMatriz(){
         matriz = new Vuelo [ciudadesOperando.length][ciudadesOperando.length][150];
         
-        
+        for (int num_vuelo = 0; num_vuelo<vuelos.length; num_vuelo++){
+            
+            String [] partes = vuelos[num_vuelo].split(",");
+            
+            for (int i = 0; i<ciudadesOperando.length; i++){
+                if (partes [1].equals(ciudadesOperando[i])){
+                        for (int j = 0; j<ciudadesOperando.length; j++){
+                        if (partes [2].equals(ciudadesOperando[j])){
+                            for (int k = 0; k<150; k++){
+                                if (matriz[i][j][k]==null){
+                                    String precio_str = partes[0];
+                                    double precio = Double.parseDouble(precio_str);
+                                    
+                                    String asientos_str = partes[8];
+                                    int asientos = Integer.parseInt(asientos_str);
+                                    
+                                    matriz[i][j][k] = new Vuelo (precio, partes [1], partes[2], partes[3], partes[4], partes[5], partes[6], partes[7], asientos);
+                                }
+                            }
+                        }
+                    }  
+                }
+            }
+        }
     }
     
-    public void VueloDirecto(){
+    public String VueloDirecto(String ciudadSalida, String ciudadLlegada, String fechaSalida){
+        double precio_mas_barato = Double.MAX_VALUE;
+        int indice_vuelo_mas_barato = -1;
+        int indice_ciudad_salida = -1;
+        int indice_ciudad_llegada = -1;
         
+        for (int i = 0; i<ciudadesOperando.length; i++){
+            if (ciudadSalida.equals(ciudadesOperando[i])){
+                indice_ciudad_salida = i;
+                for (int j = 0; j<ciudadesOperando.length; j++){
+                    if (ciudadLlegada.equals(ciudadesOperando[j])){
+                        indice_ciudad_llegada = j;
+                        for (int k = 0; k<150; k++){
+                            if (matriz[i][j][k] != null && matriz[i][j][k].getFechaSalida().equals(fechaSalida)){
+                                if (matriz[i][j][k].getPrecio()<precio_mas_barato){
+                                    precio_mas_barato = matriz[i][j][k].getPrecio();
+                                    indice_vuelo_mas_barato = k;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (indice_vuelo_mas_barato == -1){
+            return null;
+        }
+        else{
+            return matriz[indice_ciudad_salida][indice_ciudad_llegada][indice_vuelo_mas_barato].imprimirVuelo();
+        }
     }
     
     public void VueloEscalas(){
